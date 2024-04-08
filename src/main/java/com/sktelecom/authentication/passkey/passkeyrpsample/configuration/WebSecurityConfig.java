bebb1c1e985d13a5b0416b53db1d6dce61ad44fb
InGee Kim <ingee.kim@sk.com>
2024-04-08 20:18:12 +0900

    Add 'chrome-extension://bbomeihhaijmpilamfliekkmgmkanfgo' to CORs list

--- src/main/java/com/sktelecom/authentication/passkey/passkeyrpsample/configuration/WebSecurityConfig.java
+++ src/main/java/com/sktelecom/authentication/passkey/passkeyrpsample/configuration/WebSecurityConfig.java
@@ -58,7 +58,10 @@ public class WebSecurityConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("*").allowCredentials(true);
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "chrome-extension://bbomeihhaijmpilamfliekkmgmkanfgo")
+                .allowedMethods("*")
+                .allowCredentials(true);
     }
 
 //    @Bean
