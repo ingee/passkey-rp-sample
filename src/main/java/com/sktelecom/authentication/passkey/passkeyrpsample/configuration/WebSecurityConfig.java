7eda081e67164f969868f6653d60e02ab3249c49
InGee Kim <ingee.kim@sk.com>
2024-04-08 18:23:45 +0900

    Add 'http://localhost:5173' to CORs list

--- src/main/java/com/sktelecom/authentication/passkey/passkeyrpsample/configuration/WebSecurityConfig.java
+++ src/main/java/com/sktelecom/authentication/passkey/passkeyrpsample/configuration/WebSecurityConfig.java
@@ -20,15 +20,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class WebSecurityConfig {
+public class WebSecurityConfig implements WebMvcConfigurer {
     @SuppressWarnings("checkstyle:AbbreviationAsWordInName")
     @Value("${webauthn.server.oauth2-protected:false}")
     private boolean apiProtectedWithOAuth2;
@@ -42,12 +46,28 @@ public class WebSecurityConfig {
             .and()
             .csrf().disable()  // disable csrf
             .authorizeRequests()
-                .anyRequest().permitAll();
+            .anyRequest().permitAll();
         if (apiProtectedWithOAuth2) {
             http.oauth2Client();
         }
+        http.cors();
         // @formatter:on
 
         return http.build();
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("*").allowCredentials(true);
+    }
+
+//    @Bean
+//    public WebMvcConfigurer webMvcConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**").allowedOrigins("*");
+//            }
+//        };
+//    }
 }
